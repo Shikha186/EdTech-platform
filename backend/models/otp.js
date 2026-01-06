db62c90ba92d42b8c80814ddb6c2a856f2dc4bc7
Shikha186 <163021594+Shikha186@users.noreply.github.com>
2026-01-07 00:59:44 +0530

    pre middleware added to otpSchema

--- backend/models/otp.js
+++ backend/models/otp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
 
 const OTPSchema= new mongoose.Schema({
     email:{
@@ -15,4 +16,22 @@ const OTPSchema= new mongoose.Schema({
         expires:300 // OTP expires in 5 minutes
     }
 })
+
+// verification email function 
+async function sendVerificationEmail(email, otp){
+    try{
+        const mailResponse= await mailSender(email,"OTP Verification",`Your OTP for verification is ${otp}. It is valid for 5 minutes.`);
+        console.log("otp verification mail sent successfully",mailResponse);
+
+    }catch(error){
+        console.log("otp verification error while sending mail",error);
+    }
+
+};
+// pre-save hook to send OTP email
+OTPSchema.pre('save', async function(next){
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+})
+
 module.exports= mongoose.model("OTP",OTPSchema);
