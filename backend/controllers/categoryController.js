f0b5496539233f988b8558be99ee6ca27717f68c
Shikha186 <163021594+Shikha186@users.noreply.github.com>
2026-05-26 19:14:16 +0530

    updated handlers

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,7 +1,7 @@
-const Tag=require("../models/tags");
+const Category=require("../models/cayegory");
 
-// Create a new tag
-exports.createTag=async(req,res)=>{
+// Create a new category
+exports.createCategory=async(req,res)=>{
     try{
         //fetch data
         const {name, description}=req.body;
@@ -13,21 +13,21 @@ exports.createTag=async(req,res)=>{
             })
         }
         //create entry in db
-        const tagDetails= await Tag.create({
+        const categoryDetails= await Category.create({
             name:name,
             description:description
         })
 
         return res.status(200).json({
             success:true,
-            message:"tag created successfully"
+            message:"category created successfully"
         })
 
     }catch(error){
-        console.log("error in creating tag",error);
+        console.log("error in creating category",error);
         return res.status(500).json({
-            success:true,
-            messsage:error
+            success:false,
+            message:error
         })
 
 
@@ -35,13 +35,13 @@ exports.createTag=async(req,res)=>{
 }
 
 
-// getall tags handler
-exports.showALLTags=async(req,res)=>{
+// getall categories handler
+exports.showALLCategories=async(req,res)=>{
    try{
-     const getTags=await Tag.find({},{name:true, description:true});
+     const getCategories=await Category.find({},{name:true, description:true});
      return res.status(200).json({
         success:true,
-        message:"all tags are returned successfully"
+        message:"all categories are returned successfully"
      })
    }catch(error){
     console.log(error);
@@ -50,4 +50,47 @@ exports.showALLTags=async(req,res)=>{
         message:error
     })
    }
+}
+
+
+//category page details handler
+exports.categoryPageDetails=async(req,res)=>{
+    try{
+        //get category id from req body
+        const {categoryId}=req.body;
+        //get courses under that categoryid
+        const selectedCategory=await Category.findById({_id:categoryId}).populate("courses").exec();
+        //validation
+        if(!selectedCategory){
+            return res.status(404).json({
+                success:false,
+                message:"Category not found"
+            })
+        }
+        //get courses from different category 
+        const differentCategoryCourses=await Category.find({_id:{$ne:categoryId}}).populate("courses").exec();
+        //get top selling courses
+        const topSellingCoureses=await Category.find({})
+        .populate({
+            path: "courses",
+            options: {
+                sort: { studentsEnrolled: -1 },
+                limit: 10
+            }
+        }).exec();
+        //return response with all the details
+        return res.status(200).json({
+            success:true,
+            message:"category details fetched successfully",
+            selectedCategory:selectedCategory,
+            differentCategoryCourses:differentCategoryCourses,
+            topSellingCoureses:topSellingCoureses
+        });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error
+        })
+    }
 }
