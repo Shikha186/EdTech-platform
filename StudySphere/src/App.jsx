c552168406e1a475d8f943adf2a4bc39ec371e40
Shikha186 <163021594+Shikha186@users.noreply.github.com>
2026-06-13 23:07:03 +0530

    added StudySphere for frontend

---
+++ StudySphere/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
